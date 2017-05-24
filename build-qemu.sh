8d4eec31f266f37f91149d0d3dca1c08a84a1fd7
Benjamin Bimmermann <b.bimmermann@yahoo.de>
2017-05-24 15:43:09 +0200

    [ADD] Add new QEMU-Version 2.6 and more build output

--- build-qemu.sh
+++ build-qemu.sh
@@ -8,24 +8,32 @@ else
 fi
 
 if [[ $1 = "debug" ]]; then
+  echo "make debug"
   debug_flags="--enable-debug --enable-debug-tcg --enable-debug-info"
   build_dir=.dbg_build
 fi
 
 if [[ $1 = "release" ]]; then
+  echo "make release"
   build_dir=.opt_build
 fi
 
+echo "make $QSIM_PREFIX/lib/"
 mkdir -p $QSIM_PREFIX/lib/
 if [ ! -d "$build_dir" ]; then
   mkdir -p $build_dir
   cd $build_dir
+  echo "make QEMU FLAGS"
   QEMU_CFLAGS="-I${QSIM_PREFIX} -g -fPIC -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -Wno-deprecated-declarations -Wstrict-prototypes -Wredundant-decls -Wall -Wundef -Wwrite-strings -Wmissing-prototypes -fno-strict-aliasing -fno-common -Wendif-labels -Wmissing-include-dirs -Wempty-body -Wnested-externs -Wformat-security -Wformat-y2k -Winit-self -Wignored-qualifiers -Wtype-limits -fstack-protector-all -Wno-uninitialized" ../qemu/configure --extra-ldflags=-shared --target-list=aarch64-softmmu,x86_64-softmmu --disable-pie --disable-brlapi --disable-rdma --disable-rbd --disable-tcmalloc --disable-xen --disable-gtk --disable-glusterfs --disable-xfsctl --disable-uuid $debug_flags
 else
+  echo "enter $build_dir"
   cd $build_dir
 fi
 
+echo "build QEMU"
 make -j4
+echo "go back"
 cd ..
+echo "clean up"
 rm -f build
 ln -s $build_dir build
